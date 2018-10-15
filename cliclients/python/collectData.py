52329d08a9dbb2bf142fdcc31df277f65e5b1a8f
dexta <dexta.de@gmail.com>
2018-10-15 21:20:14 +0200

    add: python to came down

---
+++ cliclients/python/collectData.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os
+infocmds = {
+  "hw": {
+    "network": {
+      "eth0": {
+        "ip4": "ip -f inet -o addr show eno1|cut -d\  -f 7 | cut -d/ -f 1",
+        "ip6": "ip -6 -o addr show eth0 | cut -d\  -f 7 | cut -d/ -f 1",
+        "mac": "cat /sys/class/net/eth0/address",
+        "status": "cat /sys/class/net/eth0/operstate"
+      }
+    },
+    "cpu": {
+      "count": "grep -c ^processor /proc/cpuinfo",
+      "mhz": "lscpu | grep \"MHz\" | awk '{print $3}'"
+    },
+    "ram": {
+      "mb": "grep MemTotal /proc/meminfo | awk '{print $2}'"
+    },
+    "disk": {
+      "sda": {
+        "gb": "fdisk -l /dev/sda | grep GiB | awk '{print $3}'"
+      }
+    }
+  }
+}
+
+
+def getch():
+  return os.popen("ip -f inet -o addr show eno1|cut -d\  -f 7 | cut -d/ -f 1").read().replace("\n","")
+
+# print(getch())
+
+def getshell(cmd):
+  return os.popen(cmd).read().replace("\n","")
+
+
+def walkCMDs(cmdlist,prefix):
+  for key, item in cmdlist.items():  
+    if isinstance(item,dict):
+      walkCMDs(item,prefix+"."+key)
+    else:
+      # print(prefix+"."+key+":"+item)
+      reItem = getshell(item)
+      print(prefix+"."+key+":"+reItem)
+
+
+
+walkCMDs(infocmds,'server.lxtest01')
